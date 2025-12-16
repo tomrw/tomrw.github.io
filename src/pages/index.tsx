477d18cc04882599c64b685b0612b8f50d76dbcf
tomrw <7351557+tomrw@users.noreply.github.com>
2025-12-16 19:17:52 +0800

    add github actions

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+import type { HeadFC, PageProps } from "gatsby"
+
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+};
+
+const IndexPage: React.FC<PageProps> = () => {
+  return (
+    <main style={pageStyles}>
+      <h1 style={headingStyles}>Hi</h1>
+    </main>
+  );
+};
+
+export default IndexPage
+
+export const Head: HeadFC = () => <title>Home Page</title>
